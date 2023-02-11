8aedc341b358e2d999588f304fde10196505e418
vladimirmarych <marychev21@gmail.com>
2023-02-11 23:17:00 +0300

    Adaptee main page 90%

--- js/main.js
+++ js/main.js
@@ -1,46 +1,59 @@
-$(document).ready(function()
-{
-    InitSideNavigation();
-    InitOwlCarousel();
+$(document).ready(function () {
+  InitBurgers();
+  InitSideNavigation();
+  InitOwlCarousel();
 });
 
+function InitBurgers() {
+  $("#mobile-burger").click(function () {
+    $("#mobile-burger").toggleClass('close')
+    $(".header__bottom").toggleClass("open");
 
-function InitSideNavigation()
-{
-    const sideNavHandler = () => $('.side-nav').toggleClass("open");
-    $('.side-nav__inner').mouseenter(sideNavHandler);
-    $('.side-nav__inner').mouseleave(sideNavHandler);
-
-    $('#catalog').click(function(e){
-        e.preventDefault();
-        sideNavHandler();
-    });
+    let body = $("body");
+    if (body.attr("style") === "overflow: hidden;") body.removeAttr("style");
+    else $("body").css("overflow", "hidden");
+  });
 }
 
+function InitSideNavigation() {
+  const toggleSideNav = () => $(".side-nav").toggleClass("open");
+  const closeSideNav = () => $(".side-nav").removeClass("open");
+  const openSideNav = () => $(".side-nav").addClass("open");
 
-function InitOwlCarousel()
-{
-    $('.slider-sect__carousel').owlCarousel({
-        //loop:true,
-        margin:90,
-        nav:true,
-        dots: true,
-        navText:[
-            "<img src='img/l-arrow.png'>","<img src='img/r-arrow.png'>"
-        ],
-        responsive:{
-            0:{
-                items:1,
-            },
-            768:{
-                items:2,
-            },
-            992:{
-                items:3,
-            },
-            1200:{
-                items: 4
-            }
-        }
-    });
+  $(".side-nav__inner").mouseenter(openSideNav);
+  $(".side-nav__inner").mouseleave(closeSideNav);
+
+  $("#catalog").click(function (e) {
+    e.preventDefault();
+    toggleSideNav();
+  });
+
+  $("#burger-close__catalog").click(closeSideNav);
+}
+
+function InitOwlCarousel() {
+  $(".slider-sect__carousel").owlCarousel({
+    //loop:true,
+    margin: 90,
+    nav: true,
+    dots: true,
+    navText: ["<img src='img/l-arrow.png'>", "<img src='img/r-arrow.png'>"],
+    responsive: {
+      0: {
+        items: 1,
+      },
+      576: {
+        items: 2,
+      },
+     /* 768: {
+        items: 2,
+      },*/
+      992: {
+        items: 3,
+      },
+      1200: {
+        items: 4,
+      },
+    },
+  });
 }
